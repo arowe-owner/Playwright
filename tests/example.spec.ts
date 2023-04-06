0d3010a61a96438e27f1657ab6b1c9fec099b1d6
Alex Rowe <roweman18@gmail.com>
2023-04-06 13:04:55 -0400

    Edit Example Script

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,9 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('Has Title', async ({ page }) => {
+  await page.goto('https://www.ecstuning.com/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/ECS/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
-});
