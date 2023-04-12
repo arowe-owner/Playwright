b9a3273937a7745f51be02536ccc431e6e2be177
Alex Rowe <roweman18@gmail.com>
2023-04-12 11:52:51 -0400

    Delete Example

--- tests/example.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('Has Title', async ({ page }) => {
-  await page.goto('https://www.ecstuning.com/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/ECS/);
-});
-
