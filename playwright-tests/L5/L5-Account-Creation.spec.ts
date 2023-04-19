0e536f6f0cbd578f5c667b5d52c2217f7214aca6
Alex Rowe <roweman18@gmail.com>
2023-04-19 14:42:59 -0400

    Added Choosing a Plan for your Podcast

--- playwright-tests/L5/L5-Account-Creation.spec.ts
+++ playwright-tests/L5/L5-Account-Creation.spec.ts
@@ -16,25 +16,66 @@ test.beforeEach(async ({ page }) => {
 
 test.describe('New Account Creation', () => {
     test('Create New Customer Account', async ({page}) => {
-        await page.pause();
-
         await page.getByRole('link', { name: 'Sign Up' }).click();
 
+        //First Name
         await page.getByPlaceholder('* First Name').fill(firstName);
 
+        //Last Name
         await page.getByPlaceholder('* Last Name').fill(lastName);
 
+        //Email
         await page.getByPlaceholder('* Email').fill(uniqueEmail);
 
+        //Password
         await page.getByPlaceholder('* Password').fill(uniquePass);
  
         await page.getByLabel('Sign me up to receive Libsyn service, community, industry and podcast educational communications.').check();
 
+        //Agree to Terms and Services
         await page.getByRole('checkbox', { name: 'I accept and agree to <a href=\'https://libsyn.com/tos-policies/tos-website/\' target=\'blank\'> Libsyn’s Terms of Service </a> and <a href=\'https://libsyn.com/tos-policies/privacy-policy/\' target=\'blank\'>Privacy Policy</a>' }).check();
 
+        //Create Account Button
         await page.getByRole('button', { name: 'Create Your Account' }).click();
 
         console.log('email: ' + uniqueEmail);
         console.log('password: ' + uniquePass);
+    })
+    test('Choose Plan for new Podcast', async ({page}) => {
+        //Login to new account
+        await page.getByPlaceholder('email').fill(uniqueEmail);
+        await page.getByPlaceholder('password').fill(uniquePass);
+        await page.getByPlaceholder('password').press('Enter');
+
+        //Podcast Unique Title
+        await page.getByRole('textbox', { name: 'Podcast Title' }).fill('Test Title' + unique);
+
+        //Enter Location
+        await page.getByPlaceholder('Enter a location').fill('1000 Seville Road');
+        await page.getByText('1000 Seville RoadWadsworth, OH, USA').click();
+
+        //Enter Card Information
+        await page.frameLocator('iframe[name="recurly-element--lxRHXnJsLvHZ6oPX"]').getByPlaceholder('Card number').fill('4111 1111 1111 1111');
+        await page.frameLocator('iframe[name="recurly-element--lxRHXnJsLvHZ6oPX"]').getByPlaceholder('MM / YY').fill('02 / 25');
+        await page.frameLocator('iframe[name="recurly-element--lxRHXnJsLvHZ6oPX"]').getByPlaceholder('CVV').fill('123');
+        await page.getByRole('button', { name: 'OK' }).click();
+        await page.getByRole('textbox', { name: 'Payment Method Nickname' }).fill('TEST CARD');
+
+        //$5 / Month
+        await page.getByRole('checkbox').first().check();
+
+        /*
+        //$7 / Month
+        await page.getByRole('checkbox').nth(1).check();
+
+        //$15 / Month
+        await page.getByRole('checkbox').nth(2).check();
+
+        //$20 / Month
+        await page.getByRole('checkbox').nth(3).check();
+        */
+
+        //Save
+        await page.getByRole('button', { name: 'Save' }).click();
     })
 })
